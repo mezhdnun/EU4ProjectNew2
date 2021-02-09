2c3e592c297b8a25d3849f9d587c6086f94d99be
Mecnun Deniz <mecnundeniz@gmail.com>
2021-02-09 14:04:43 +0000

    c

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello");
+        System.out.println("buralarda c den birseyler yaziyorum");
     }
 }
